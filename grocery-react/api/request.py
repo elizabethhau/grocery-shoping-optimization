3fdb8eb612f3cd8f8e819983b1eb241baf092143
Elizabeth Hau <ehau@wayfair.com>
2020-08-01 01:06:13 -0400

    Create basic form to add ingredients and recipes & send it to python

---
+++ grocery-react/api/request.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify, send_from_directory, Response
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods = ['POST'])
+def result(path):
+    input = request.get_json()
+    print(input)
+    userName = input['userName']
+    ingredients = input['ingredients']
+    recipes = input['recipes']
+    return jsonify(input)
+    # return Response("requested method is: " + request.method + ' user name is: ' + userName)
+    if request.method == 'POST':
+        console.log('recognized POST method')
--- grocery-react/src/GroceryForm.js
+++ grocery-react/src/GroceryForm.js
@@ -1,23 +1,121 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Multiselect } from 'multiselect-react-dropdown';
 // import {FormControl, Form} from 'react-bootstrap'
+import {Form, Button} from 'semantic-ui-react';
 
 const GroceryForm = props => {
-    const [username, setUsername] = useState(props.username ? props.username : '')
+  const TYPE_INGREDIENT = "ingredient"
+  const TYPE_RECIPE = "recipe"
+  const [username, setUsername] = useState(props.username ? props.username : '')
+  const ingredientOptions = [{ name: 'Apple', id: 1, type: TYPE_INGREDIENT }, { name: 'Banana', id: 2, type: TYPE_INGREDIENT }, { name: 'Ice cream', id: 3, type: TYPE_INGREDIENT }]
+  const recipeOptions = [
+    { name: 'Garlic Chicken (Butter, Chicken breast, Garlic powder, Onion powder, salt)', id: 1, type: TYPE_RECIPE },
+    { name: 'Butternut Squash (Butternut squash, Olive Oil, Garlic, Salt, Ground Black Pepper)', id: 2, type: TYPE_RECIPE },
+    { name: 'Brown Butter-Basted Steak (thick bone-in rib eye, salt, ground pepper, rosemary, garlic, unsalted butter)', id: 3, type: TYPE_RECIPE }
+  ]
+  const [selectedIngredients, setSelectedIngredients] = useState([])
+  const [selectedRecipes, setSelectedRecipes] = useState([])
 
-    const myChangeHandler = (e) => {
-        setUsername(e.target.value)
+  const onSelect = (selectedList, selectedItem) => {
+    console.log('got here')
+    const typeUpdated = selectedItem.type
+    console.log(selectedItem.type)
+    console.log('selectedValues', selectedIngredients)
+    console.log('selected list', selectedList)
+    console.log('selected item type', typeUpdated)
+    // setSelectedValues
+
+    if (typeUpdated === TYPE_INGREDIENT) {
+      setSelectedIngredients(selectedList)
+    } else if (typeUpdated === TYPE_RECIPE) {
+      setSelectedRecipes(selectedList)
     }
+    console.log('selectedValues', selectedIngredients)
+  }
 
-    return (
-      <form>
+  const onRemove = (removedList, removedItem) => {
+    console.log('in remove')
+    console.log('removed list', removedList)
+    console.log('removed item', removedItem)
+    const typeUpdated = removedItem.type
+
+    if (typeUpdated === TYPE_INGREDIENT) {
+      const updatedList = selectedIngredients.filter((item) => { return item.id !== removedItem.id })
+      setSelectedIngredients(updatedList)
+    } else if (typeUpdated === TYPE_RECIPE) {
+      const updatedList = selectedRecipes.filter((item) => { return item.id !== removedItem.id })
+      setSelectedRecipes(updatedList)
+    }
+  }
+
+  return (
+    <Form>
       {username ? 'Hello, ' + username : ''}
       <p>Enter your name:</p>
-      <input
+      <Form.Field>
+        <input
         type='text'
-        onChange={myChangeHandler}
+        onChange={e => setUsername(e.target.value)}
       />
-      </form>
-    );
+      </Form.Field>
+      <Form.Field>
+        <div className="flex-box">
+        <Multiselect
+        options={ingredientOptions} // Options to display in the dropdown
+        selectedValues={selectedIngredients} // Preselected value to persist in dropdown
+        onSelect={onSelect} // Function will trigger on select event
+        onRemove={onRemove} // Function will trigger on remove event
+        displayValue="name" // Property name to display in the dropdown options
+        placeholder="Select items to purchase"
+        id="ingredients"
+      />
+      </div>
+      </Form.Field>
+
+      <Form.Field>
+        <div className="flex-box">
+        <Multiselect
+        options={recipeOptions} // Options to display in the dropdown
+        selectedValues={selectedRecipes} // Preselected value to persist in dropdown
+        onSelect={onSelect} // Function will trigger on select event
+        onRemove={onRemove} // Function will trigger on remove event
+        displayValue="name" // Property name to display in the dropdown options
+        placeholder="Select recipes to purchase"
+        id="recipes"
+      />
+      </div>
+      </Form.Field>
+
+      <Form.Field>
+        <Button onClick={async () => {
+          const requestData = {
+            userName: username,
+            ingredients: selectedIngredients,
+            recipes: selectedRecipes
+          }
+          const response = await fetch('/api/request', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData)
+          })
+
+          if (response.ok) {
+            console.log('response successful')
+            const result = await response.text()
+            console.log(result)
+            console.log(JSON.parse(result))
+
+          } else {
+            console.log('response failed')
+          }
+
+        }}>Submit</Button>
+      </Form.Field>
+    </Form>
+
+  );
 }
 
 export default GroceryForm
