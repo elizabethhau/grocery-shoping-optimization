64a367110ac20ab2aba20ade8d9ecc5d71b0652e
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2020-08-05 23:46:59 -0400

    Get the form to work with recipes & show estimated time required

--- grocery-react/src/GroceryForm.js
+++ grocery-react/src/GroceryForm.js
@@ -8,16 +8,22 @@ const GroceryForm = props => {
   const TYPE_RECIPE = "recipe"
   const [username, setUsername] = useState(props.username ? props.username : '')
   const itemOptions = Constants.items
-  const recipeOptions = [
-    { name: 'Garlic Chicken (Butter, Chicken breast, Garlic powder, Onion powder, salt)', id: 1, type: TYPE_RECIPE },
-    { name: 'Butternut Squash (Butternut squash, Olive Oil, Garlic, Salt, Ground Black Pepper)', id: 2, type: TYPE_RECIPE },
-    { name: 'Brown Butter-Basted Steak (thick bone-in rib eye, salt, ground pepper, rosemary, garlic, unsalted butter)', id: 3, type: TYPE_RECIPE }
-  ]
+  const recipeOptions = Constants.recipes
+  // [
+  //   { name: 'Garlic Chicken (Butter, Chicken breast, Garlic powder, Onion powder, salt)', id: 1, type: TYPE_RECIPE },
+  //   { name: 'Butternut Squash (Butternut squash, Olive Oil, Garlic, Salt, Ground Black Pepper)', id: 2, type: TYPE_RECIPE },
+  //   { name: 'Brown Butter-Basted Steak (thick bone-in rib eye, salt, ground pepper, rosemary, garlic, unsalted butter)', id: 3, type: TYPE_RECIPE }
+  // ]
   const [selectedIngredients, setSelectedIngredients] = useState([])
   const [selectedRecipes, setSelectedRecipes] = useState([])
-  const [outputInstructions, setOutputInstructions] = useState([])
+  // const [outputInstructions, setOutputInstructions] = useState([])
   const [submitted, setSubmitted] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
+  // const [estimatedTime, setEstimatedTime] = useState()
+  const [optimizeResult, setOptimizeResult] = useState({
+    estimatedTime: null,
+    instructions: []
+  })
 
   const onSelect = (selectedList, selectedItem) => {
     const typeUpdated = selectedItem.type
@@ -46,21 +52,24 @@ const GroceryForm = props => {
   }
 
   useEffect(() => {
-    setOutputInstructions(outputInstructions)
-  }, [outputInstructions])
+    // setOutputInstructions(outputInstructions)
+    setOptimizeResult(optimizeResult)
+  }, [optimizeResult])
 
   return (
     <Container>
       <Form>
         <h3>{username ? 'Hello, ' + username : ''}</h3>
-        <p>Enter your name:</p>
+        <p>What is your name?</p>
         <Form.Field>
           <input
             type='text'
             onChange={e => setUsername(e.target.value)}
+            placeholder='name'
           />
         </Form.Field>
         <Form.Field>
+          <p>Select specific items</p>
           <Multiselect
             options={itemOptions} // Options to display in the dropdown
             selectedValues={selectedIngredients} // Preselected value to persist in dropdown
@@ -73,12 +82,13 @@ const GroceryForm = props => {
         </Form.Field>
 
         <Form.Field>
+          <p>Select from existing recipes</p>
           <Multiselect
             options={recipeOptions} // Options to display in the dropdown
             selectedValues={selectedRecipes} // Preselected value to persist in dropdown
             onSelect={onSelect} // Function will trigger on select event
             onRemove={onRemove} // Function will trigger on remove event
-            displayValue="name" // Property name to display in the dropdown options
+            displayValue="recipe" // Property name to display in the dropdown options
             placeholder="Select recipes to purchase"
             id="recipes"
           />
@@ -108,8 +118,12 @@ const GroceryForm = props => {
               console.log(JSON.parse(result))
               result = JSON.parse(result)
               // setOutputUserName(result.userName)
-              setOutputInstructions(result.instructions)
+              // setEstimatedTime(result.time)
+              console.log('TIME IS')
+              console.log(result.time)
+              // setOutputInstructions(result.instructions)
               // setOutputRecipes(result.recipes)
+              setOptimizeResult(result)
               setIsLoading(false)
               console.log(result.instructions)
 
@@ -122,27 +136,45 @@ const GroceryForm = props => {
         </Form.Field>
       </Form>
 
-      {submitted && username && (outputInstructions) ?
+      {submitted && username && (optimizeResult) ?
         <Card fluid color='yellow' >
           <Card.Content header={`Shopping Instructions for ${username}`} />
           <Card.Content>
             <Card.Description>
+              {optimizeResult.time && <p>Estimated time required: {optimizeResult.time} minutes</p>}
               <List bulleted>
-                {outputInstructions && outputInstructions.map(step => {
-                  console.log('got in output ingredients map!!')
-                  console.log(step)
+                {optimizeResult && optimizeResult.instructions.map(step => {
+                  // console.log('got in output ingredients map!!')
+                  // console.log(step)
                   return (
                     <Fragment>
                       <List.Item key={step.section}>{step.section}</List.Item>
                       <List.List>
                         {
                           step.items.map(item => {
-                            console.log('got in sub list')
-                            console.log(item)
-                            return (
+                            // console.log('got in sub list')
+                            // console.log(item)
+                            // if(selectedRecipes.length > 0) {
+                            //   selectedRecipes.forEach(recipe => {
+                            //   console.log('got in items recipe sub list!')
+                            //   const ingredients = recipe.name
+                            //   console.log(item + ingredients)
+                            //   if (ingredients.includes(item)) {
+                            //     return (<li>{item}({recipe.recipe})</li>)
+                            //   } 
+                            //   return (
+                            //     // <List.Item key={item} content={item}/>
+                            //     <li>{item}</li>
+                            //   )
+                              
+                            // })
+                            // } else {
+                              return (
                               // <List.Item key={item} content={item}/>
                               <li>{item}</li>
                             )
+                            // }
+                            
                           })
                         }
                       </List.List>
